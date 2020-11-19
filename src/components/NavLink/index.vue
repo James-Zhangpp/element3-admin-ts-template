1bb1da804c48470c408763f13fb9c25002bd1994
wxy <wxy306533309@163.com>
2020-11-19 11:47:42 +0800

    创建element3-admin-ts-template 项目

---
+++ src/components/NavLink/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <a v-if="isExternalLink" v-bind="$attrs" :href="to" target="_blank">
+    <slot />
+  </a>
+  <router-link v-else v-slot="{ isActive, href, navigate }" v-bind="$props" custom>
+    <a
+      v-bind="$attrs"
+      :href="href"
+      :class="isActive ? activeClass : inactiveClass"
+      @click="navigate"
+    >
+      <slot />
+    </a>
+  </router-link>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { RouterLink } from 'vue-router'
+
+export default defineComponent({
+  name: 'AppLink',
+
+  props: {
+    // add @ts-ignore if using TypeScript
+    ...RouterLink.props,
+    inactiveClass: String,
+  },
+
+  computed: {
+    isExternalLink() {
+      return typeof this.to === 'string' && this.to.startsWith('http')
+    },
+  },
+})
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-createApp(App)
-  .use(store)
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+import '@/styles/index.scss' // global css
+
+import 'element3/lib/theme-chalk/index.css'
+import Element3 from 'element3'
+
+import { useIcons } from '@/icons' // icon
+import '@/permission' // permission control
+
+const app = createApp(App)
   .use(router)
-  .mount("#app");
+  .use(store)
+  .use(Element3)
+useIcons(app)
+app.mount('#app')
